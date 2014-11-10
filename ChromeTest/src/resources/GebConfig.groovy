0a93831e03ec8ba43f1471d6dd07523d5aba4400
Piyush <piyush19jan@gmail.com>
2014-11-11 00:02:28 +0530

    New Experiment

---
+++ ChromeTest/src/resources/GebConfig.groovy
@@ -0,0 +1,38 @@
+mport com.google.common.collect.ImmutableMap
+import org.openqa.selenium.chrome.ChromeDriver
+import org.openqa.selenium.chrome.ChromeDriverService
+import org.openqa.selenium.firefox.FirefoxDriver
+import org.openqa.selenium.phantomjs.PhantomJSDriver
+
+waiting {
+    timeout = 2
+}
+
+environments {
+
+    // run via “./gradlew chromeTest”
+    // See: http://code.google.com/p/selenium/wiki/ChromeDriver
+    chrome {
+        File chromeDriverLocation = new File("./resources/chromedriver_LINUX_64")
+        ChromeDriverService chromeDriverService   = new ChromeDriverService.Builder()
+                .usingDriverExecutable(chromeDriverLocation)
+                .usingAnyFreePort().withEnvironment(ImmutableMap.of("DISPLAY", ":1")).build()
+        chromeDriverService.start()
+        driver = { new ChromeDriver(chromeDriverService) }
+    }
+
+    // run via “./gradlew firefoxTest”
+    // See: http://code.google.com/p/selenium/wiki/FirefoxDriver
+    firefox {
+        driver = { new FirefoxDriver() }
+    }
+
+    // phantomJs {
+    //     driver = { new PhantomJSDriver() }
+    // }
+
+}
+
+// To run the tests with all browsers just run “./gradlew test”
+
+baseUrl = "http://gebish.org"
